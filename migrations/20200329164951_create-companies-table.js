d1220ad78b5826bd31542bef286a230454a11066
Amy Kim <amesk3@gmail.com>
2020-03-29 18:11:20 -0400

    basic db setup

---
+++ migrations/20200329164951_create-companies-table.js
@@ -0,0 +1,18 @@
+exports.up = knex =>
+  knex.schema.createTable('companies', table => {
+    table
+      .uuid('id')
+      .primary()
+      .defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('name').notNullable();
+    table.string('headline') 
+    table.string('address') 
+    table.string('state') 
+    table.string('city') 
+    table.string('country') 
+    table.string('zip_code')
+    table.timestamps(true, true);
+  });
+
+
+exports.down = knex => knex.schema.dropTable('companies');
